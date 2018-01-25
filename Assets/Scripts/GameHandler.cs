994af355fcc7e072387eadaf753c77309ee8bba3
superbandit <bor@flamikaan.nl>
2018-01-25 22:59:53 +0100

    forgot to commit this
    oops

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -9,6 +9,9 @@ public class GameHandler : MonoBehaviour
 
     public Player player;
 
+    private bool removeEnemies = false;
+    private GameObject[] enemies; 
+
 	void Awake ()
     {
         if(Instance == null)
@@ -20,19 +23,64 @@ public class GameHandler : MonoBehaviour
         {
             Destroy(gameObject);
         }
-
-	}
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
 
     private void Start()
     {
-        player = GameObject.Find("Player").GetComponent<Player>();
+
     }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        if (removeEnemies == true)
+        {
+            RemoveEnemies();
+        }
+    }
+
 
     // Update is called once per frame
     void Update ()
     {
-		
+		// check dem cheats
+        if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                removeEnemies = !removeEnemies;
+                RemoveEnemies();
+            }
+
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                player.invincible = !player.invincible;
+            }
+
+            if (Input.GetKeyDown(KeyCode.KeypadMinus) || Input.GetKeyDown(KeyCode.Minus))
+            {
+                if (player.speed > 0)
+                    player.speed -= 1;
+            }
+
+            if (Input.GetKeyDown(KeyCode.KeypadPlus) || Input.GetKeyDown(KeyCode.Equals))
+            {
+                if (player.speed < 10)
+                    player.speed += 1;
+            }
+        }
 	}
+
+    private void RemoveEnemies()
+    {
+        foreach(GameObject e in enemies)
+        {
+            Destroy(e);
+        }
+    }
 
     public void NextLevel()
     {
